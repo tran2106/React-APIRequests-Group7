fed880514a0e41bfb99975ea3fbc8ed863a19348
Tran Vo <vobaohoangtran@gmail.com>
2025-06-24 13:46:50 -0400

    added API key

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 // import axios from "axios"; // Maybe we'll need axios? 🤔
+import SearchField from "./SearchField";
+import GifCard from "./GifCard";
 import "./style.css";
 
 // const GIPHY_API_KEY = "YOUR_API_KEY";
+const GIPHY_API_KEY = "3OSO9K8tBsI2u8bUlgc6Qg6ZgwyJ2Lrp";
 
 const App = () => {
   return (
@@ -17,3 +20,4 @@ const App = () => {
 // it into the index.html
 const root = createRoot(document.getElementById("root"));
 root.render(<App />);
+
